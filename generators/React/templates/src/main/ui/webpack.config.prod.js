5df9b562fe65d7d65d52809f943746e38f1fa9da
Mike Freeman <mfreeman@xtivia.com>
2017-09-10 07:58:33 -0500

    Fix build break for React portlets due to wrong NODE_ENV replacement config

--- generators/React/templates/src/main/ui/webpack.config.prod.js
+++ generators/React/templates/src/main/ui/webpack.config.prod.js
@@ -21,7 +21,7 @@ module.exports = {
       manifest: require(path.join(__dirname,'react_runtime','react_runtime-manifest.json'))
     }),
     // avoid issues in things like React-Router that test for the value of NODE_ENV
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': 'production' })
+    new webpack.DefinePlugin({'process.env': {NODE_ENV: JSON.stringify('production')}}),
   ],
   module: {
     loaders: [
